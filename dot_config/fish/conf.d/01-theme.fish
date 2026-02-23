3b3f5facc626a86a576cf5d710a520a4d642a069
Koury Lape <klape@roadrunnerwm.com>
2026-02-23 15:33:02 +0000

    Fix theme

--- dot_config/fish/conf.d/01-theme.fish
+++ dot_config/fish/conf.d/01-theme.fish
@@ -1,14 +1,8 @@
-if not set -q FISH_THEME
-  set -g FISH_THEME "rose-pine"
-end
-
-# Path to themes directory
-set -l theme_path "$HOME/.config/fish/themes/$FISH_THEME.fish"
-
-# Check if the theme file exists and source it
-if test -f $theme_path
-    source $theme_path
-else
-  # Try to load it
-  fish_config theme choose $FISH_THEME
+if status is-interactive
+  if not set -q FISH_THEME
+    set -g FISH_THEME "rose-pine"
+  end
+  if test -f "$HOME/.config/fish/themes/$FISH_THEME.theme"
+    fish_config theme choose $FISH_THEME
+  end
 end
